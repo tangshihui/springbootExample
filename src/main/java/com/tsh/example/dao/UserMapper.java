837fb36cf2f2d5b766116e45ca2b70cce5b60d6e
tangshihui <761034176@qq.com>
2018-05-17 22:42:37 +0800

    add global exception

--- src/main/java/com/tsh/example/dao/UserMapper.java
+++ src/main/java/com/tsh/example/dao/UserMapper.java
@@ -1,11 +1,11 @@
 package com.tsh.example.dao;
 
 import com.tsh.example.model.User;
-import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
+//@Mapper
 public interface UserMapper {
     void save(User user);
     User getById(Integer id);
