b764e1687d653775b970db7f3039e14e5ef346f6
tangshihui <761034176@qq.com>
2018-05-17 22:41:33 +0800

    add global exception

--- src/main/java/com/tsh/example/controller/HomeController.java
+++ src/main/java/com/tsh/example/controller/HomeController.java
@@ -3,24 +3,27 @@ package com.tsh.example.controller;
 import com.tsh.example.model.User;
 import com.tsh.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/home/{homeId}")
 public class HomeController {
 
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "hello,world";
+    @RequestMapping(value = "/hello/{detailId}", method = RequestMethod.GET)
+    public String hello(@PathVariable(value = "homeId",required = false) String homeId,@PathVariable( value="detailId",required = false) String detailId) {
+        return "hello,world!" + "homeId:" + homeId + (detailId != null ? "detailId:" + detailId : "");
     }
 
     @RequestMapping(value = "/getuser", method = RequestMethod.GET)
-    public User getUser(){
+    public User getUser(@PathVariable( value = "homeId",required = false) String homeId){
+
+
 
         return userService.getUser();
     }
